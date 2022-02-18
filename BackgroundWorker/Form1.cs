c429ae8d6222ea1bd43c91d1a8a910480d7c517d
Sri Manikanda Prasath.S <79691102+srimanikandaprasath@users.noreply.github.com>
2022-02-18 13:23:39 +0800

    BackgroundWorker

---
+++ BackgroundWorker/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorker_Tut
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker workerThread = null;
+        
+
+        bool _keepRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
