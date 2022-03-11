d4244da905a91d51ee600e8c0e69802421f07e5d
Sri Manikanda Prasath.S <79691102+srimanikandaprasath@users.noreply.github.com>
2022-03-11 16:53:54 +0800

    Encapsulation_Tut

---
+++ Encapsulation_Tut/DataPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Tut
+{
+    class DataPacket
+    {
+        public DataPacket()
+        {
+
+        }
+
+        public string sTestGroup { get; set; }
+        public string sPC_No { get; set; }
+        public string sConnection { get; set; }
+        public string nParaCount { get; set; }
+        public string sCommand_S0 { get; set; }
+        public string sCommand_S1 { get; set; }
+        public string sCommand_Gen { get; set; }
+    }
+}
---
+++ Encapsulation_Tut/NextClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Tut
+{
+    class NextClass
+    {
+        DataPacket obj2 = new DataPacket();
+        public void test1()
+        {
+            obj2.sCommand_Gen = "Gencommand-2";
+            //Console.WriteLine(obj2.sCommand_Gen);
+        }
+       
+    }
+}
