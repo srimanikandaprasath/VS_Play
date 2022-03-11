6a263eeeb4d37520e4ed10ba7230af879f0d0bf3
Sri Manikanda Prasath.S <79691102+srimanikandaprasath@users.noreply.github.com>
2022-03-11 16:54:38 +0800

    Lambda_Generic_Tut

---
+++ Lambda_Generic_Tut/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Generic_Tut
+{
+    class Hero
+    {
+        public Hero()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HeroName { get; set; }
+        public bool CanFly { get; set; }
+    }
+}
